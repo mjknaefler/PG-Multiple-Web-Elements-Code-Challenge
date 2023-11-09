6fc9ec2afca52245704c0acc084378010dda5fc7
mknaefler <mjknaefler@gmail.com>
2023-11-09 11:14:13 -0800

    Small change to code

--- test/testcases.js
+++ test/testcases.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 describe('Test cases for practicing multiple web elements', async () => {
-    it('Verify there are more than 40 links on the facebook.com', async() => {
+    it.only('Verify there are more than 40 links on the facebook.com', async() => {
         /**
          * 1. Launch facebook.com 
          * 2. Verify there are more than 40-links
@@ -23,11 +23,11 @@ describe('Test cases for practicing multiple web elements', async () => {
 
         //1. Launch https://classroomessentialsonline.com/  
         await browser.url('https://classroomessentialsonline.com/  ');
-        await browser.pause(3000);
+        
         //2. Move mouse to Church Chairs
         const churchChairsElement = await $('//a[@href="/church-chairs/"]');
         await churchChairsElement.moveTo();
-        await browser.pause(3000);
+        
         //3. Verify there are 7-options in Church Chairs
         const churchChairOptions = await $$('//a[@href="/church-chairs/"]/following-sibling::div[@id="navPages-"]//li');
         expect(churchChairOptions.length,'There are not 7 church chair options').to.equal(7);
